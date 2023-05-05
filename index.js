7e449c71b332fe53116b8d2dbee4db395534d2d2
Anna <34191811+404unf@users.noreply.github.com>
2023-05-05 10:30:56 -0700

    Add HTML, CSS, and JS files

---
+++ index.js
@@ -0,0 +1,6 @@
+const reference = document.getElementById('reference')
+
+const link = document.createElement('a')
+link.href = 'https://www.livewellbakeoften.com/classic-banana-bread-recipe/'
+
+document.getElementsByTagName('h5')[0].appendChild(link)
